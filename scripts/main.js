38c81fca8ebfd9f9947da6b23d1a0cd05e9b2971
nineuito <nineuito@gmail.com>
2023-03-21 15:04:33 +0200

    revision

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,2 @@
-let number = 5;
-
-console.log(number);
-
-const sum = 8;
-
-console.log(sum);
-
-let text = 'Hello';
-
-console.log(text);
-
-let logic = true;
-
-console.log(logic);
-
-let arr = [6, 3, 32, 'Ноутбук', []];
-
-console.log(arr[3]);
-
-
-const obj = {
-    name: 'игорь',
-    height: 180
-}
-
-console.log(obj.name);
+const result = confirm('Вам есть 18 лет?');
+console.log(result);
