b08e05dfeb750162ff7731a743ef4b284f53d4f1
nineuito <nineuito@gmail.com>
2023-03-21 13:49:49 +0200

    revision

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,22 @@ const sum = 8;
 
 console.log(sum);
 
+let text = 'Hello';
+
+console.log(text);
+
+let logic = true;
+
+console.log(logic);
+
+let arr = [6, 3, 32, 'Ноутбук', []];
+
+console.log(arr[3]);
+
+
+const obj = {
+    name: 'игорь',
+    height: 180
+}
+
+console.log(obj.name);
