5641a729216194afb0d086852fd71d0f01f7a77d
nineuito <nineuito@gmail.com>
2023-03-26 20:53:26 +0300

    Revert "revision"
    This reverts commit 5514b2cb02385ec34f3617e04b798f49e0998c93.

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,25 @@
-const newPost = (post, addedAt = Date()) => ({
-    ...post,
-    addedAt: addedAt
-})
+// function multByFactor(value, multiplier) {
+//     if (multiplier === undefined) {
+//         multiplier = 1;
+//     }
+//     return value * multiplier
+// }
 
-const firstPost = {
-    id: 1,
-    author: 'Oleh'
+// console.log(multByFactor(10,2))
+// console.log(multByFactor(10))
+
+// const multByFactor = function(value, multiplier) {
+//     if (multiplier === undefined) {
+//         multiplier = 1;
+//     }
+
+//     return value * multiplier;
+// }
+
+// console.log(multByFactor(2,6))
+
+const multByFactor = (value, multiplier = 1) => {
+    return value * multiplier;
 }
 
-console.table(newPost(firstPost))
+console.log(multByFactor(6))
