5514b2cb02385ec34f3617e04b798f49e0998c93
nineuito <nineuito@gmail.com>
2023-03-26 20:20:31 +0300

    revision

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,25 @@
-const result = {
+// function multByFactor(value, multiplier) {
+//     if (multiplier === undefined) {
+//         multiplier = 1;
+//     }
+//     return value * multiplier
+// }
 
+// console.log(multByFactor(10,2))
+// console.log(multByFactor(10))
+
+// const multByFactor = function(value, multiplier) {
+//     if (multiplier === undefined) {
+//         multiplier = 1;
+//     }
+
+//     return value * multiplier;
+// }
+
+// console.log(multByFactor(2,6))
+
+const multByFactor = (value, multiplier = 1) => {
+    return value * multiplier;
 }
 
-console.log(!!typeof(result))
+console.log(multByFactor(6))
